0b4ad95331811b025a457cabdb5c472d91882b3a
taibislamdipu <mailtaibislam@gmail.com>
2021-11-11 16:34:20 +0600

    product delete api done

--- index.js
+++ index.js
@@ -89,7 +89,21 @@ async function run() {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await ordersCollection.deleteOne(query);
-            res.json(result)
+            res.json(result);
+
+            if (result.deletedCount === 1) {
+                console.log("Successfully deleted one document.");
+            } else {
+                console.log("No documents matched the query. Deleted 0 documents.");
+            }
+        })
+
+        // delete product from the database
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productsCollection.deleteOne(query)
+            res.json(result);
 
             if (result.deletedCount === 1) {
                 console.log("Successfully deleted one document.");
